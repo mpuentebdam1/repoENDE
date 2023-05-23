a011c33e2dd6fe5f46359875761c20f1f69c7281
Jagobainda <63314576+jagobainda@users.noreply.github.com>
2023-05-23 19:34:02 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Lo primero de todo, cómo están los máquinas?");
+        System.out.println("Venga va, vámos a sacarnos una foto.");
     }
 }
